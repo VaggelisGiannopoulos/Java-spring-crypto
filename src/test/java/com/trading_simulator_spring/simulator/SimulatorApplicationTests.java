9dc0bc86a9d861d029417c19594e965ce4ae974d
Vaggelis <baggelis100@gmail.com>
2024-02-14 21:20:25 +0200

    Initial commit

---
+++ src/test/java/com/trading_simulator_spring/simulator/SimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading_simulator_spring.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
